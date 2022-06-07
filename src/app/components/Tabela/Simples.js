804bcca03a988ce46a59f4d04313f7d5ffe7847a
WarleyEduardo <warleyeduardo@hotmail.com>
2022-06-07 09:21:30 -0300

    correção

---
+++ src/app/components/Tabela/Simples.js
@@ -0,0 +1,48 @@
+/*
+  Modulo 23 - Dashboard  Finalizando  tela de pedidos 1/3	
+*/
+
+import React from 'react';
+const TabelaSimples = ({ cabecalho, dados }) => {
+	
+	<div className='TabelaSimples'>
+		<table className='Simples'>
+			<thead>
+				<tr>
+					{cabecalho.map((item, idx) => (<th key={idx}>{item}</th>) )}
+				</tr>
+			</thead>
+			<tbody>
+				{
+					dados.map((linha, idx) => (
+						<tr key={idx}>
+							{
+								Object.keys(cabecalho).map((item, index) =>
+								 (
+								    <td key={index}>{linha[item] || ""}</td>
+								    	
+								))								
+							}
+							{
+								linha["botaoDetalhes"] && (
+									<td>
+										<Link to={linha["botaoDetalhes"]}>
+											<button>
+												DETALHES
+											</button>
+										</Link>
+									</td>
+								 )
+							}
+					  </tr>
+				  ))
+				}
+			</tbody>
+
+		</table>
+	  
+  </div>
+
+};
+
+export default TabelaSimples;
--- src/app/containers/Pedidos/index.js
+++ src/app/containers/Pedidos/index.js
@@ -6,35 +6,67 @@
 import React, { Component } from 'react';
 import Titulo from '../../components/Texto/Titulo'
 import Pesquisa from '../../components/Inputs/Pesquisa'
+import moment from 'moment';
+
+/*
+  Modulo 23 - Dashboard  Finalizando  tela de pedidos 1/3	
+*/
+
+import Tabela from '../../components/Tabela/Simples';
 
 class Pedidos extends Component {
-	
 	state = {
-		pesquisa: ""
-	}
+		pesquisa: '',
+	};
 
 	onChangePesquisa = (ev) => this.setState({ pesquisa: ev.target.value });
-	 
+
+	/*
+		  Modulo 23 - Dashboard  Finalizando  tela de pedidos 2/3	
+	   */
+	
+	
 
 	render() {
-
+		const { pesquisa } = this.state;
 		/*
 		  Modulo 23 - Dashboard  Finalizando  tela de pedidos 1/3	
 	   */
-		const { pesquisa } = this.state;
+		
+		const dados = [
+			{
+				Cliente: 'Cliente 1',
+				'Valor Total': 89.9,
+				Data: moment().toISOString(),
+				Situação: 'Aguardando Pagamento',
+				botaoDetalhes: '/pedido/9D1DS345FFD9',
+			},
+			{
+				Cliente: 'Cliente 2',
+				'Valor Total': 105.9,
+				Data: moment().toISOString(),
+				Situação: 'Aguardando Pagamento',
+				botaoDetalhes: '/pedido/CCDDFS345DFD55',
+			},
+			{
+				Cliente: 'Cliente 3',
+				'Valor Total': 26.72,
+				Data: moment().toISOString(),
+				Situação: 'Pagamento Concluído',
+				botaoDetalhes: '/pedido/834545DFDFDFDF',
+			},
+		];
+		
 
 		return (
 			<div className='Pedidos'>
 				<div className='card'>
 					<Titulo tipo='h1' titulo='Pedidos' />
 					<br />
-					<Pesquisa
-						valor={pesquisa}
-						placeholder={"Pesquise aqui pelo nome do cliente..."}
-						onChange={ (ev)=> this.onChangePesquisa(ev)} />
-					
+					<Pesquisa valor={pesquisa} placeholder={'Pesquise aqui pelo nome do cliente...'} onChange={(ev) => this.onChangePesquisa(ev)} />
+
 					<br />
-					<Tabela />
+					<Tabela cabecalho={['Cliente', 'Valor Total', 'Data', 'Situação']} dados={dados} />
 					<Paginacao />
 				</div>
 			</div>
